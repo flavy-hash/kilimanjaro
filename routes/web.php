72110653251081ca94c4948e1600bb9ce7ecde2c
flavy-hash <flavianmichael663@gmail.com>
2026-09-14 14:10:26 +0300

    adding the routes on the web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Http\Controllers\InquiryController;
+use App\Http\Controllers\TripController;
+use App\Models\Package;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('welcome', ['trips' => Package::forSite()]);
+})->name('home');
+
+Route::get('/tours', function () {
+    return view('tours', ['trips' => Package::forSite()]);
+})->name('tours');
+
+Route::post('/inquiries', [InquiryController::class, 'store'])->name('inquiries.store');
+
+Route::get('/{trip}', [TripController::class, 'show'])
+    ->where('trip', '[a-z0-9-]+')
+    ->name('trip.show');
