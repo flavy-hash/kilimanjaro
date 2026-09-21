50324b5b2f1a6ead877edbb995d79e7c8c9d0dab
flavy-hash <flavianmichael663@gmail.com>
2026-09-21 16:27:38 +0300

    updating the route for the newpage created

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,29 @@
 <?php
 
 use App\Http\Controllers\InquiryController;
+use App\Http\Controllers\ReviewController;
 use App\Http\Controllers\TripController;
+use App\Models\HomeContent;
 use App\Models\Package;
+use App\Models\Review;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome', ['trips' => Package::forSite()]);
+    return view('welcome', [
+        'trips' => Package::forSite(),
+        'featuredReviews' => Review::approved()->featured()->with('package')->latest('stayed_at')->take(3)->get(),
+        'home' => HomeContent::current(),
+    ]);
 })->name('home');
 
 Route::get('/tours', function () {
     return view('tours', ['trips' => Package::forSite()]);
 })->name('tours');
+
+// Declared before the catch-all "/{trip}" route below, which would
+// otherwise swallow "/reviews" as if it were a package slug.
+Route::get('/reviews', [ReviewController::class, 'index'])->name('reviews.index');
+Route::post('/reviews', [ReviewController::class, 'store'])->name('reviews.store');
 
 Route::post('/inquiries', [InquiryController::class, 'store'])->name('inquiries.store');
 
